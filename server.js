5886701dc82c40cbd907ceedf36a7dbd4efaf059
siddhantsonar36 <siddhantsonar36@gmail.com>
2024-05-01 23:07:54 +0530

    updated configs

--- server.js
+++ server.js
@@ -28,6 +28,6 @@ if (process.env.NODE_ENV === 'production') {
   });
 }
 
-const PORT = process.env.PORT || 5000;
+const PORT = require('config').get('PORT') || 5000;
 
 app.listen(PORT, () => console.log(`server started on port ${PORT}`));
